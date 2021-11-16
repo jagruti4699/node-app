76c16356a93acbe66ca78e24f9a2b6db8890ea77
jagruti4699 <80021688+jagruti4699@users.noreply.github.com>
2021-11-16 15:01:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:red;">My first AWS codepipeline task!</h1> \n');
+  res.send('<h1 style="color:green;">My first AWS codepipeline task!</h1> \n');
 });
 
 app.listen(PORT, HOST);
